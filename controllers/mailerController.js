386f072d300b66a4717ee2bdb07db2be1521b79b
maiquegouveia <maiquebusiness@gmail.com>
2023-06-27 22:56:21 -0300

    fixed bad import of nodeMailer

--- controllers/mailerController.js
+++ controllers/mailerController.js
@@ -1,4 +1,4 @@
-const nodeMailer = require("../nodeMailer");
+const nodeMailer = require("../nodemailer");
 
 exports.sendConfirmationCode = async (req, res) => {
   const { email, codeClient } = req.body;
