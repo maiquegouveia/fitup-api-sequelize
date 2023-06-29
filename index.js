99488bbe9b8ddad6b54c273dc3eebceb32ef819c
maiquegouveia <maiquebusiness@gmail.com>
2023-06-29 15:48:57 -0300

    added cors middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const userRouter = require("./routes/userRoutes");
 const foodCategoryRouter = require("./routes/foodCategoryRoutes");
@@ -13,6 +14,7 @@ const commentRoutes = require("./routes/commentRoutes");
 const app = express();
 
 // app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v3/users", userRouter);
